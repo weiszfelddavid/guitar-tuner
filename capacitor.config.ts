6641e051d69b76744cb40d96764f0c1f2e16798a
weiszfelddavid <weiszfelddavid@users.noreply.github.com>
2026-01-01 07:26:18 +0000

    Chore: Project Scaffold

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.guitartuner.pro',
+  appName: 'GuitarTuner',
+  webDir: 'dist'
+};
+
+export default config;
