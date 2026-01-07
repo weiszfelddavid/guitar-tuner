fd047e461ab7ef7505a1908a37475da09a360aa0
weiszfelddavid <weiszfelddavid@users.noreply.github.com>
2026-01-07 01:07:23 +0000

    chore: wipe repository to start from scratch

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.guitartuner.pro',
-  appName: 'GuitarTuner',
-  webDir: 'dist'
-};
-
-export default config;
